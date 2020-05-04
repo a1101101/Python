3e55819b1d810d185a726730b528c5c36f14d9e1
a1101101 <r3901@icloud.com>
2020-05-04 20:12:39 +0900

    Add files via upload

---
+++ algorithm/binsearch.py
@@ -0,0 +1,32 @@
+#二分探索
+
+def inp():
+    n,k = input().split()
+    n,k = int(n),int(k)
+    
+    A = input().split()
+    A = [int(A[i]) for i in range(n)]
+    return A,k
+
+def binsch(A,k):
+    n = len(A)
+    if n==1:
+        if A[0]==k:
+            return 0
+        else:
+            return -1
+    else:
+        B = A[:n//2]
+        C = A[n//2:]
+        b = binsch(B,k)
+        c = binsch(C,k)
+        if b!=-1:
+            return b
+        elif c!=-1:
+            return c+len(B)
+        else:
+            return -1
+
+if __name__ == '__main__':
+    A,k = inp()
+    print(binsch(A,k))
